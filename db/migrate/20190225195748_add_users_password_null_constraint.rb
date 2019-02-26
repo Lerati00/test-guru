134c1a5abd3e9843020264a0f48676252417e468
lerati <nuda1234@bk.ru>
2019-02-26 10:34:01 +0200

    Added foreign keys and changed the type of answers.correct

--- db/migrate/20190225195748_add_users_password_null_constraint.rb
+++
@@ -1,5 +0,0 @@
-class AddUsersPasswordNullConstraint < ActiveRecord::Migration[5.2]
-  def change
-    change_column_null(:users, :password, false)
-  end
-end
--- db/migrate/20190225200104_add_answers_correct_default_value.rb
+++
@@ -1,5 +0,0 @@
-class AddAnswersCorrectDefaultValue < ActiveRecord::Migration[5.2]
-  def change
-    change_column_default(:answers, :correct, false)
-  end
-end
