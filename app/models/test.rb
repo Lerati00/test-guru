e404c8c4005be104c7dc87ab83d8f68e1d29d382
lerati <nuda1234@bk.ru>
2019-02-27 13:58:04 +0200

    ActiveRecord Query Interface

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,2 +1,11 @@
 class Test < ApplicationRecord
+  belongs_to :category
+  has_many :completed_tests
+
+  def self.categories_with_title(title)
+    Test.joins(:category)
+        .where("categories.title = ?", title)
+        .order(title: :desc)
+        .pluck(:title)
+  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,32 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+User.create(login: 'ss', password: 'ss')
+
+Category.create([
+  { title: 'Разное' },
+  { title: 'Исскуство' },
+  { title: 'Спорт' },
+  { title: 'Наука' }
+])
+
+Test.create([
+  { category_id: 1, title: 'Просто тест' },
+  { category_id: 1, title: 'Супер тест' },
+  { category_id: 1, title: 'Тест' }
+])
+
+Question.create([
+  { test_id: 1, body: "Из какого предмета кухонного обихода отлили стойкого оловянного солдатика из сказки Андерсена?" },
+  { test_id: 1, body: "Как называется линия, изображающая процесс, происходящий без теплообмена с окружающей средой?" },
+  { test_id: 1, body: 'Какое изысканное блюдо принесли на обед отбывавшему срок в пятнадцать суток Верзиле в комедии Л.Гайдая "Операция Ы"?' },
+  { test_id: 1, body: "Какой знак восточного гороскопа следует за знаком Дракона?" },
+  { test_id: 1, body: 'Кто ввел в русский алфавит букву "Ё"?' },
+  { test_id: 1, body: "Как звали жену Рембрандта?" }
+])
+
+Answer.create([
+  { question_id: 1 },
+  { question_id: 1 },
+  { question_id: 3 },
+  { question_id: 4 },
+  { question_id: 5 },
+  { question_id: 6 }
+])
