76cf5b3ab806616acaaef440e4a24db4cf8d82de
lerati <nuda1234@bk.ru>
2019-03-07 13:48:59 +0200

    task7 -- rails views 1/4

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,31 +1,53 @@
 class QuestionsController < ApplicationController
-  before_action :find_test
-  before_action :find_question, only: %i[show destroy]
+  before_action :find_test, only: %w[index new create]
 
   rescue_from ActiveRecord::RecordNotFound, with: :rescue_with_question_not_found
 
   def index
+    @questions = Question.all
   end
 
   def show
+    @question = Question.find(params[:id])
   end
 
   def new
+    @question = @test.questions.new
+  end
+
+  def edit
+    @question = Question.find(params[:id])
   end
 
   def create
-    @question = @test.questions.create(question_attribute)
-    redirect_to test_question_path(@test, @question)
+    @question = @test.questions.new(params_question)
+
+    if @question.save
+      redirect_to question_path(@question)
+    else
+      render plain: 'fail'
+    end
+  end
+
+  def update
+    @question = Question.find(params[:id])
+
+    if @question.update(params_question)
+      redirect_to question_path(@question)
+    else
+      render plain: 'fail'
+    end
   end
 
   def destroy
+    @question = Question.find(params[:id])
     @question.destroy
-    redirect_to test_questions_path
+    redirect_to test_path(@question.test)
   end
 
   private
 
-  def question_attribute
+  def params_question
     params.require(:question).permit(:body)
   end
 
@@ -33,11 +55,7 @@ class QuestionsController < ApplicationController
     @test = Test.find(params[:test_id])
   end
 
-  def find_question
-    @question = Question.find(params[:id]) 
-  end
-
   def rescue_with_question_not_found
-    render plain: "Question not found!"
+    head 404
   end
 end
--- app/helpers/tests_helper.rb
+++ app/helpers/tests_helper.rb
@@ -1,2 +1,10 @@
 module TestsHelper
+  def test_level(test)
+    level = test.level
+    if level.between?(0, 3)
+      %w[easy elementary advanced hard][level] 
+    else
+      "hard"
+    end
+  end
 end
