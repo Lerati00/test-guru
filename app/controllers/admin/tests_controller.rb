08c1de4b75f9387e84e393c3a1d17c3f36f319e1
lerati <nuda1234@bk.ru>
2019-04-02 11:40:47 +0300

    task15 -- query

--- app/controllers/admin/tests_controller.rb
+++ app/controllers/admin/tests_controller.rb
@@ -1,10 +1,10 @@
 class Admin::TestsController < Admin::BaseController
-  before_action :find_test, only: %i[show destroy edit update start]
+  before_action :set_tests, only: %i[index update_inline]
+  before_action :find_test, only: %i[show destroy edit update start update_inline]
 
   rescue_from ActiveRecord::RecordNotFound, with: :rescue_with_test_not_found
 
   def index
-    @tests = Test.all
   end
 
   def show
@@ -42,6 +42,14 @@ class Admin::TestsController < Admin::BaseController
     redirect_to admin_tests_path
   end
 
+  def update_inline
+    if @test.update(test_params)
+      redirect_to admin_tests_path
+    else
+      render :index
+    end
+  end
+
   private
 
   def test_params
@@ -54,5 +62,9 @@ class Admin::TestsController < Admin::BaseController
 
   def rescue_with_test_not_found
     redirect_to '/404/'
+  end
+
+  def set_tests
+    @tests = Test.all
   end
 end
--- app/helpers/test_passages_helper.rb
+++ app/helpers/test_passages_helper.rb
@@ -1,2 +1,5 @@
 module TestPassagesHelper
+  def progress_percent(number, total)
+    ((number* 100)/total) 
+  end
 end
